b43d52faf83d6d0d95e99c061f0d4addd2d86cdc
Geon Youn <geon.youn@outlook.com>
2023-02-07 14:12:17 -0500

    Add styling

--- script.js
+++ script.js
@@ -17,17 +17,37 @@ function addBookToLibrary(...args) {
 
 function displayBooks() {
     book_container.textContent = "";
-    for (let book of myLibrary) {
-        div = document.createElement("div");
+    for (let idx in myLibrary) {
+        const book = myLibrary[idx];
+        const div = document.createElement("div");
         div.classList.add("book");
-
-        for (let prop in book) {
-            inner = document.createElement("div");
-            inner.classList.add(prop);
-            inner.textContent = book[prop];
-            div.append(inner);
+        div.textContent = `"${book.title}" by "${book.author}" (${book.n_pages} pages)`;
+        if (book.read) {
+            div.classList.add("read");
         }
 
+        const div2 = document.createElement("div");
+        const btn = document.createElement("button");
+        btn.setAttribute("data-idx", idx);
+        btn.classList.add("remove-book");
+        btn.addEventListener("click", (e) => {
+            myLibrary.splice(e.target.getAttribute("data-idx"), 1);
+            displayBooks();
+        });
+        btn.textContent = "Remove";
+        div2.append(btn);
+
+        const btn2 = document.createElement("button");
+        btn2.setAttribute("data-idx", idx);
+        btn2.classList.add("toggle-read");
+        btn2.addEventListener("click", (e) => {
+            const i = e.target.getAttribute("data-idx");
+            myLibrary[i].read = !myLibrary[i].read;
+            displayBooks();
+        });
+        btn2.textContent = book.read ? "Not Read" : "Read";
+        div2.append(btn2);
+        div.append(div2);
         book_container.append(div);
     }
 }
@@ -35,8 +55,9 @@ function displayBooks() {
 book_form = document.querySelector("#book_form");
 
 new_book = document.querySelector("#new_book");
-new_book.addEventListener("click", () => {
+new_book.addEventListener("click", (e) => {
     book_form.removeAttribute("style");
+    e.target.setAttribute("style", "display: none;");
 });
 
 book_form.addEventListener("submit", (e) => {
@@ -53,6 +74,7 @@ book_form.addEventListener("submit", (e) => {
     et.n_pages.value = "";
     et.read.checked = false;
     et.setAttribute("style", "display: none;");
+    new_book.removeAttribute("style");
 });
 
 displayBooks();
